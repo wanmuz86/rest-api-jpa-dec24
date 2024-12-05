0dda2575e980df9a38111925959fab9ef4bcc654
Wan Muzaffar <wanmuz86@gmail.com>
2024-12-05 14:50:55 +0800

    add custom query

--- src/main/java/com/example/JPADemo/controller/UserController.java
+++ src/main/java/com/example/JPADemo/controller/UserController.java
@@ -3,7 +3,9 @@ package com.example.JPADemo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +30,16 @@ public class UserController {
 	@PostMapping
 	public User createUser(@RequestBody User user) {
 		return userService.saveUser(user);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteUser(@PathVariable long id) {
+		userService.deleteUser(id);
+	}
+	
+	@GetMapping("/{name}")
+	public List<User> getUserByName(@PathVariable String name){
+		return userService.findUsersByName(name);
 	}
 
 }
--- src/main/java/com/example/JPADemo/repository/UserRepository.java
+++ src/main/java/com/example/JPADemo/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.example.JPADemo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.JPADemo.model.User;
@@ -12,4 +14,24 @@ import com.example.JPADemo.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 
+	
+	// JPA Query builder Methods
+	// Reference: https://docs.spring.io/spring-data/jpa/reference/repositories/query-methods-details.html
+	
+	// SELECT * FROM user WHERE name LIKE '%Muzaffar%'
+	List<User> findByNameContaining(String name);
+	
+	// SELECT * FROM user WHERE age > 18
+	List<User> findByAgeGreaterThan(int age);
+	
+	
+	// SELECT * FROM user WHERE name="Muzaffar" AND email="wanwan@gmail.com"
+	List<User> findByNameAndEmail(String name, String email);
+	
+	
+	//SELECT * FROM user WHERE age < 18
+	List<User> findByAgeLessThan(int age);
+	
+	
+	
 }
